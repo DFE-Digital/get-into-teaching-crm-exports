38b0dcf0ce17c689c1f7b552fdcd36840b266edb
Peter Yates <peter.yates@graphia.co.uk>
2021-09-09 10:29:48 +0100

    Add documentation for the views
    The documentation is inline in the form of comments that describe the
    columns in the views, the tables from which we pull data and details on
    why/how we're doing things.
    
    There are a couple of `TODO` notes with questions about what the
    underlying data means which will allow us to confirm
    suspicions/assumptions.

--- views/event_registrations.sql
+++ views/event_registrations.sql
@@ -1,34 +1,66 @@
--- create view git.event_registrations as (
-select
-    top 1000
-    er.Id as id,
-    er.msevtmgt_eventid as event_id,
-    er.msevtmgt_contactid as contact_id,
-    case
-    when c.id is not null 
-        then 'yes'
-    when e.msevtmgt_eventenddate > dateadd(week, -1, getdate())
-        -- the event was recent (or hasn't happened yet), 
-        -- there's still time for attendence to be confirmed
-        then 'unknown'
-    else 'no'
-    end as attended,
-    convert(smalldatetime, er.createdon) as registered_on,
-    convert(smalldatetime, c.createdon) as attendence_confirmed_on,
-    md.LocalizedLabel as creation_channel
+create view git.event_registrations as (
+	-- shows a full list of event registrations. A registration
+	-- is a record that joins an event with a contact and potentially
+	-- a checkin.
+	--
+	-- TODO: there appears to be a 1:0..1 relationship between registration
+	--       and checkin and I can't work out why
+	select
+		-- the event registration id (guid)
+		er.id as id,
 
-from
-    crm_msevtmgt_eventregistration er
-left outer join
-    crm_msevtmgt_checkin c 
-        on er.Id = c.msevtmgt_registrationid
-inner join
-    crm_msevtmgt_event e 
-        on er.msevtmgt_eventid = e.Id
-inner join 
-    crm_OptionSetMetadata md
-        on er.dfe_channelcreation = md.[Option]
-        and md.OptionSetName = 'dfe_channelcreation'
+		-- event id, matches git.events.id (guid)
+		er.msevtmgt_eventid as event_id,
 
-order by
-    e.msevtmgt_eventenddate desc
+		-- contact id, matches contact_id in mailing list and tta views (guid)
+		er.msevtmgt_contactid as contact_id,
+
+		-- the attendance status of the registration
+		-- when 'yes' the registrant attended
+		-- when unknown:
+		--      * the event hasn't happened yet
+		--      * the event happened in the last week so there's still
+		--        an opportunity for the registrant to confirm attendance
+		-- when 'no' the registrant did not attend
+		case
+		when ci.id is not null
+			then 'yes'
+		when e.msevtmgt_eventenddate > dateadd(week, -1, getdate())
+			then 'unknown'
+		else 'no'
+		end as attended,
+
+		-- the date the registration was made (e.g., 2021-05-30)
+		convert(smalldatetime, er.createdon) as registered_on,
+
+		-- the date the attendance was confirmed (e.g., 2021-06-12)
+		convert(smalldatetime, ci.createdon) as attendance_confirmed_on,
+
+		-- creation channel pulled from common lookup table via 'dfe_ChannelCreation'
+		cc.LocalizedLabel as creation_channel
+
+	from
+		-- raw event registrations from dynamics
+		crm_msevtmgt_eventregistration er
+
+	left outer join
+		-- raw checkins list from dynamics. may or may not exist so left outer
+		-- join to include both attended and not-attended registrations
+		crm_msevtmgt_checkin ci
+			on er.Id = ci.msevtmgt_registrationid
+
+	inner join
+		-- raw events listing from Dynamics
+		crm_msevtmgt_event e
+			on er.msevtmgt_eventid = e.Id
+
+	inner join
+		-- dynamics central EAV lookup
+		crm_OptionSetMetadata cc
+			on er.dfe_ChannelCreation = cc.[Option]
+			and cc.OptionSetName = 'dfe_channelcreation'
+			and cc.EntityName = 'msevtmgt_eventregistration'
+
+	order by
+		e.msevtmgt_eventenddate desc
+);
--- views/teacher_training_advisers.sql
+++ views/teacher_training_advisers.sql
@@ -1,36 +1,54 @@
--- create view as git.teacher_training_adviser _signups as (
-select
-    top 100
-    c.id as contact_id,
-    c.dfe_gitisttaservicestartdate as signed_up_at,
-    tta_subscription_channel_lookup.LocalizedLabel as subscription_channel,
+create view as git.teacher_training_adviser_signups as (
+	-- shows all contacts who have signed up for a TTA
+	select
+		-- contact id, matches contact_id in event_registrations, mailing list views etc (guid)
+		c.id as contact_id,
 
-    case c.dfe_gitisttaservicedonotemail
-        when 0 then 'no'
-        when 1 then 'yes'
-        else null 
-    end as opted_out_of_all_emails,
+		-- the date and time the signup was completed
+		convert(smalldatetime, c.dfe_gitisttaservicestartdate) as signed_up_at,
 
-    case c.dfe_gitismailinglistservicedonotbulkemail
-        when 0 then 'no'
-        when 1 then 'yes'
-        else null 
-    end as opted_out_of_bulk_emails,
+		-- subscription channel pulled from the common lookup table
+		-- channels include things like 'on campus service', 'social media',
+		-- 'grad fairs', 'pop-up events' etc
+		sc.LocalizedLabel as subscription_channel,
 
-    case c.dfe_gitisttaservicedonotpostalmail
-        when 0 then 'no'
-        when 1 then 'yes'
-        else null 
-    end as opted_out_of_post
+		-- TODO: clarify the relationship between opted_out_of_all_emails
+		--       and opted_out_of_bulk_emails. Does the former include the
+		--       latter? (i.e. is our 'all' in the column name accurate?)
 
+		-- has the contact opted out of all TTA emails?
+		case c.dfe_gitisttaservicedonotemail
+			when 0 then 'no'
+			when 1 then 'yes'
+			else null
+		end as opted_out_of_all_emails,
 
-from
-    crm_contact c
+		-- has the contact opted out of all TTA bulk emails?
+		case c.dfe_gitismailinglistservicedonotbulkemail
+			when 0 then 'no'
+			when 1 then 'yes'
+			else null
+		end as opted_out_of_bulk_emails,
 
-left outer join
-    crm_OptionSetMetadata tta_subscription_channel_lookup
-        on c.dfe_gitisttaservicesubscriptionchannel = tta_subscription_channel_lookup.[Option]
-        and tta_subscription_channel_lookup.optionsetname = 'dfe_gitisttaservicesubscriptionchannel'
+		-- has the contact opted out post?
+		case c.dfe_gitisttaservicedonotpostalmail
+			when 0 then 'no'
+			when 1 then 'yes'
+			else null
+		end as opted_out_of_post
 
-where
-    c.dfe_gitisttaservicestartdate is not null;
+	from
+		-- dynamics primary list of contacts
+		crm_contact c
+
+	left outer join
+		-- dynamics central EAV lookup
+		crm_OptionSetMetadata sc
+			on c.dfe_gitisttaservicesubscriptionchannel = sc.[Option]
+			and sc.optionsetname = 'dfe_gitisttaservicesubscriptionchannel'
+			and sc.EntityName = 'contact'
+
+	where
+		-- only target users who've actually signed up for a TTA
+		c.dfe_gitisttaservicestartdate is not null
+);
