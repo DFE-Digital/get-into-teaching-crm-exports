51d6cab11eeb3a792009d136f5c265eba5e71929
Peter Yates <peter.yates@graphia.co.uk>
2022-02-22 15:38:32 +0000

    Make the list of included countries dynamic

--- views/profile.sql
+++ views/profile.sql
@@ -1,4 +1,16 @@
-alter view git.profile as (
+alter view git.profile as
+    with countries_with_600_candidates as (
+        select
+            c.dfe_country
+        from
+            crm_contact c
+        where
+            c.createdon >= '2019-01-01'
+        group by 
+            c.dfe_country
+        having
+            count(*) >= 600
+    )
     select
         -- contact id, the unique identifier for the candidate
         c.id,
@@ -74,23 +86,7 @@ alter view git.profile as (
         -- most frequent candidate countries, don't return any
         -- with just a few candidates so we can't identify anyone
         case
-            when country.dfe_name in (
-                'United Kingdom',
-                'India',
-                'Nigeria',
-                'United States',
-                'South Africa',
-                'China',
-                'Pakistan',
-                'Spain',
-                'Hong Kong',
-                'Ghana',
-                'France',
-                'United Arab Emirates',
-                'Italy',
-                'Republic Of Ireland',
-                'Germany'
-            )
+            when c.dfe_country in (select * from countries_with_600_candidates)
             then country.dfe_name
         else
             'Other'
@@ -134,5 +130,4 @@ alter view git.profile as (
 
     where
         c.createdon >= '2019-01-01'
-
-);
+;
