09c0841685606db8009694e1cff1b141d16187bb
Peter Yates <peter.yates@graphia.co.uk>
2022-03-09 12:36:05 +0000

    Fix application dates
    This splits application_started and application_dates. I'd wrongly assumed that the source table was holding complete applications but it's holding a mix and we can use the completion date to separate them.

--- views/applications.sql
+++ views/applications.sql
@@ -8,14 +8,26 @@ alter view git.applications as (
         -- contact id, matches contact_id in event_registrations, tta views etc (guid)
         af.dfe_contact as contact_id,
 
-        -- when was the application made, both datetime and time
-        convert(smalldatetime, af.createdon) as applied_at,
-        convert(date, af.createdon) as applied_on,
+        -- when was the application started, both datetime and time
+        convert(smalldatetime, af.createdon) as application_started_at,
+        convert(date, af.createdon) as application_started_on,
 
         -- application phase, there appear to be two values:
         -- * Apply 1 (the first application)
         -- * Apply 2 (any subsequent application)
         phase.localizedlabel as phase,
+
+        ry.localizedlabel as recruitment_year,
+
+        -- when the application was completed, both datetime and time
+        af.dfe_submittedatdate as applied_at,
+        convert(date, af.dfe_submittedatdate) as applied_on,
+
+        -- flag that tells us whether this is a complete application
+        case
+            when af.dfe_submittedatdate is null then 0
+            else 1
+        end as application_complete,
 
         -- application status, these are states from Apply's state machine
         --
@@ -38,4 +50,11 @@ left outer join
         on af.dfe_applystatus = status.[Option]
         and status.optionsetname = 'dfe_candidateapplystatus'
         and status.EntityName = 'contact' 
+
+left outer join
+    -- dynamics central EAV lookup (application status)
+    crm_OptionSetMetadata ry
+        on af.dfe_recruitmentyear = ry.[Option]
+        and ry.optionsetname = 'dfe_recruitmentyear'
+        and ry.EntityName = 'dfe_applyapplicationform' 
 );
