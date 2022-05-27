9f775ba4965770b0c5ca2e7142b83f4de9a890ed
Peter Yates <peter.yates@graphia.co.uk>
2022-05-27 14:40:51 +0100

    Set a varchar length. SQL Server defaults to 1!

--- marketing-pass/matches.sql
+++ marketing-pass/matches.sql
@@ -3,23 +3,23 @@ drop table if exists fuzzy.matches;
 create table fuzzy.matches
 (
     -- apply fields
-    apply_email_address varchar,
-    apply_first_name varchar,
-    apply_last_name varchar,
-    apply_date_of_birth varchar,
-    apply_postcode varchar,
-    appy_phone varchar,
+    apply_email_address varchar(300),
+    apply_first_name varchar(300),
+    apply_last_name varchar(300),
+    apply_date_of_birth varchar(300),
+    apply_postcode varchar(300),
+    appy_phone varchar(300),
 
     -- crm fields
-    crm_email_address varchar,
-    crm_first_name varchar,
-    crm_last_name varchar,
-    crm_date_of_birth varchar,
-    crm_postcode varchar,
-    crm_phone varchar,
+    crm_email_address varchar(300),
+    crm_first_name varchar(300),
+    crm_last_name varchar(300),
+    crm_date_of_birth varchar(300),
+    crm_postcode varchar(300),
+    crm_phone varchar(300),
 
     -- extra info and match type
-    apply_id varchar,
-    crm_id varchar,
+    apply_id varchar(300),
+    crm_id varchar(300),
     match_type int
 )
